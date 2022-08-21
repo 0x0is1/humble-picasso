1f4975f54dd88d5b2dd85e5f1ec99fb105b7819c
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-08-21 18:44:48 +0530

    [+] Basic process completed

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from picasso_bot import *
 import os
 from utils import wsv
 
-auth_token = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+auth_token = os.environ.get('BOT_TOKEN')
 wsv.wsv()
 bot.run(auth_token)
